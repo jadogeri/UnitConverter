007e9582e8748daffe4599da3fca3ca38bcafcea
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2024-12-12 09:30:54 -0600

    Modified TemperatureTest.py

--- server/tests/TemperatureTest.py
+++ server/tests/TemperatureTest.py
@@ -1,55 +1,17 @@
 
-
-import pytest
 import requests
 import unittest
 import sys
-from pytest_mock import MockFixture
 
 from os.path import dirname, abspath
 dir = dirname(dirname(abspath(__file__)))
-print("path ===",dir)
-
-dir_models = dir +'\\models'
-print(dir_models)
-
 dir_tests = dir +'\\tests';
-
-
-sys.path.append(dir_models)
 sys.path.append(dir_tests)
+sys.path.append(dir)
+from constants import *
+print(base_url)
 
-
-base_url = "http://localhost:5000/api"
-print("calling time........................................................")
-# def test_url():
-#     # Arrange:
-#     seconds = 60
-#     minutes = 60
-#     hour = 24
-#     #Act:
-#     response = make_request()
-#     print("response object ===", response)
-#     #Assertion:
-#     assert response.status_code == 200  # Validation of status code  
-#     data = response.json()  
-#     # Assertion of body response content:  
-#     assert len(data) > 0  
-#     assert data["element_count"] > 0
-
-
-def make_request(val):
-    # response = requests.get(f'{base_url}?api_key={api_key}')
-    response = requests.get(f'{base_url}/time/secondsToMinutes?seconds={val}')
-    print(response.json())
-
-    return response.json()
-
-
-
-# from utils.utils import *
-
-class APITest(unittest.TestCase):
+class TemperatureTest(unittest.TestCase):
 
    
     def setUp(self):
@@ -57,46 +19,141 @@ class APITest(unittest.TestCase):
         print("SETUP called ...");
         # Arrange 
 
-        self.num1 : int = 60;
-        self.num2 : int = 2;
-        self.num3 : int = 5;
-        self.num4 : int = -3;
+        self.celcius = -12.22
+        self.celcius2 = -272.15
+        self.fahrenheit = 50.0
+        self.kelvin = 283.15
 
-
-     
     def tearDown(self) :
         print("TEARDOWN called ...");
+        self.celcius = 0.0
+        self.celcius2 = 0.0
+        self.fahrenheit = 0.0
+        self.kelvin = 0.0
 
-        self.num1 = 60.0;
-        self.num2 = 0;
-        self.num3 = 0;
-        self.num4 = 0;
-
-
-     
-
-    def test_seconds_to_minutes(self):
+    def test_celcius_to_fahrenheit(self):
         #Act
-        result = make_request(self.num1)
-        print("print statement",result)
-        #Assert
-        self.assertEqual(result, 1.0)
-'''
-    def test_correct_fibonacci(self):
+        # result = make_request(self.num1)
+
+        try:
+            #Act
+            response = requests.get(f'{base_url}/temperature/celciusToFahrenheit?celcius={self.celcius}')
+
+            if response.status_code == 200:
+                print(response.status_code)
+                result = response.json();
+            #Assert
+                self.assertEqual(result, 10.0)
+            else:
+                print('Error:',response.status_code,response.json())
+                return response
+
+        except requests.exceptions.RequestException as e:
+            print('Error:==', e)
+            return e
+
+    def test_celcius_to_kelvin(self):
         #Act
-        result = fibonacci(self.num3)
-        #Assert
-        self.assertEqual(result, 3)
-    
-    def test_is_valid_input(self):
+        # result = make_request(self.num1)
+
+        try:
+            #Act
+            response = requests.get(f'{base_url}/temperature/celciusToKelvin?celcius={self.celcius2}')
+
+            if response.status_code == 200:
+                print(response.status_code)
+                result = response.json();
+            #Assert
+                self.assertEqual(round(result), 1.0)
+            else:
+                print('Error:',response.status_code,response.json())
+                return response
+
+        except requests.exceptions.RequestException as e:
+            print('Error:==', e)
+            return e
+        
+    def test_fahrenheit_to_celcius(self):
         #Act
-        result = is_valid_input(self.num4)
-        #Assert
-        self.assertFalse(result);    
+        # result = make_request(self.num1)
 
-'''
+        try:
+            #Act
+            response = requests.get(f'{base_url}/temperature/fahrenheitToCelcius?fahrenheit={self.fahrenheit}')
 
+            if response.status_code == 200:
+                print(response.status_code)
+                result = response.json();
+            #Assert
+                self.assertEqual(result, 10.0)
+            else:
+                print('Error:',response.status_code,response.json())
+                return response
 
+        except requests.exceptions.RequestException as e:
+            print('Error:==', e)
+            return e
+        
+    def test_fahrenheit_to_kelvin(self):
+        try:
+            #Act
+            response = requests.get(f'{base_url}/temperature/fahrenheitToKelvin?fahrenheit={self.fahrenheit}')
 
+            if response.status_code == 200:
+                print(response.status_code)
+                result = response.json();
+            #Assert
+                self.assertEqual(result, 283.15)
+            else:
+                print('Error:',response.status_code,response.json())
+                return response
+
+        except requests.exceptions.RequestException as e:
+            print('Error:==', e)
+            return e
+        
+    def test_kelvin_to_fahrenheit(self):
+            #Act
+        try:
+            #Act
+            response = requests.get(f'{base_url}/temperature/kelvinToFahrenheit?kelvin={self.kelvin}')
+
+            if response.status_code == 200:
+                print(response.status_code)
+                result = response.json();
+            #Assert
+                self.assertEqual(round(result), 50.0)
+            else:
+                print('Error:',response.status_code,response.json())
+                return response
+
+        except requests.exceptions.RequestException as e:
+            print('Error:==', e)
+            return e
+
+    def test_kelvin_to_celcius(self):
+        #Act
+        # result = make_request(self.num1)
+
+        try:
+            #Act
+            response = requests.get(f'{base_url}/temperature/kelvinToCelcius?kelvin={self.kelvin}')
+
+            if response.status_code == 200:
+                print(response.status_code)
+                result = response.json();
+            #Assert
+                self.assertEqual(result, 10.0)
+            else:
+                print('Error:',response.status_code,response.json())
+                return response
+
+        except requests.exceptions.RequestException as e:
+            print('Error:==', e)
+            return e
+        
+ 
+ 
 if __name__ == "__main__":
     unittest.main()
+
--- server/tests/tests.py
+++ server/tests/tests.py
@@ -1,7 +1,9 @@
 
-# from GeometryTest import *
-# from TimeTest import *
+from GeometryTest import *
+from TimeTest import *
 from CookingTest import *
+from TemperatureTest import *
+
 
 if __name__ == "__main__":
     unittest.main()
